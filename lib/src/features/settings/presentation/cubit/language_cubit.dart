536aae498fd98ff5ad404aafa1cfd76b2c3fad90
Arvind Kaushik <arvindkaushik.kaushik90@gmail.com>
2026-01-10 19:46:53 +0530

    remove null check and store null in toJson

--- lib/src/features/settings/presentation/cubit/language_cubit.dart
+++ lib/src/features/settings/presentation/cubit/language_cubit.dart
@@ -7,27 +7,19 @@ class LanguageCubit extends HydratedCubit<Locale?> {
 
   void setLocale(Locale? locale) => emit(locale);
 
-  Locale? get resolvedLocale {
-    if (state == null) return null;
-    return state;
-  }
-
   @override
   Locale? fromJson(Map<String, dynamic> json) {
     final code = json[GlobalConstant.languageCode] as String?;
     if (code == null) return null;
-
     final countryCode = json[GlobalConstant.countryCode] as String?;
     return Locale(code, countryCode);
   }
 
   @override
   Map<String, dynamic>? toJson(Locale? state) {
-    if (state == null) return null;
-
     return {
-      GlobalConstant.languageCode: state.languageCode,
-      GlobalConstant.countryCode: state.countryCode,
+      GlobalConstant.languageCode: state?.languageCode,
+      GlobalConstant.countryCode: state?.countryCode,
     };
   }
 }
